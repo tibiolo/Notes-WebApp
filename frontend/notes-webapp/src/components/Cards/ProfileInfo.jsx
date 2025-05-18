65a6cb641c89955cb23028235f52fb581a4bdb44
tibiolo <makso544g@gmail.com>
2025-05-18 16:58:41 +0100

    Minor adjustments to how data/functions are passed to ProfileInfo component

--- frontend/notes-webapp/src/components/Cards/ProfileInfo.jsx
+++ frontend/notes-webapp/src/components/Cards/ProfileInfo.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { getInitials } from '../../utils/helper';
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = (props) => {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-200">
-        {getInitials("Maksymilian Szymanski")}
+        {getInitials('Maksymilian Szymanski')}
       </div>
       <div className="">
         <p className="text-sm font-medium">Maks</p>
-        <button className="text-sm text-slate-700 underline" onClick={onLogout}>
+        <button
+          className="text-sm text-slate-700 underline"
+          onClick={props.onLogout}
+        >
           Logout
         </button>
       </div>
