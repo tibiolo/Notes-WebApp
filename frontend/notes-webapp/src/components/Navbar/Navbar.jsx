2b2725b293ef55ae79b56bf3fbb15a5813ae8519
tibiolo <makso544g@gmail.com>
2025-05-18 17:00:46 +0100

    Added SearchBar component logic to Navbar

--- frontend/notes-webapp/src/components/Navbar/Navbar.jsx
+++ frontend/notes-webapp/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ProfileInfo from '../Cards/ProfileInfo';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
-  const navigate = useNavigate;
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
 
   const onLogout = () => {
     navigate('/login');
   };
 
+  const handleSearch = () => {};
+
+  const onSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery('');
+  };
+
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={onSearch}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
 
       <ProfileInfo onLogout={onLogout} />
     </div>
