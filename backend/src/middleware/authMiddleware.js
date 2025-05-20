2c11dd4d7aaa63d31e0ea85242166c6865e34d07
tibiolo <makso544g@gmail.com>
2025-05-20 18:26:40 +0100

    Implemented authMiddleware for verifying json web tokens

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+// Loading JSON TOKEN SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid or expired token' });
+  }
+};
