d6e09c9aa7689c68d4ff7a7bf600a61221ad3d86
tibiolo <makso544g@gmail.com>
2025-05-21 02:46:19 +0100

    implemented noteController

---
+++ backend/src/controllers/noteController.js
@@ -0,0 +1,34 @@
+import {
+  getNotes,
+  saveNote,
+  editNote,
+  deleteNote,
+  updatePinNote,
+} from '../models/noteModel.js';
+
+export const fetchNotes = async (req, res) => {
+  const user_id = req.user.user_id;
+
+  try {
+    const result = await getNotes(user_id);
+
+    return res.json(result);
+  } catch (err) {
+    console.error('Error fetching notes', err);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+export const createNote = async (req, res) => {
+  const { title, context, pinned, tags } = req.body;
+  const user_id = req.user.user_id;
+
+  try {
+    const result = await saveNote(user_id, title, context, pinned, tags);
+
+    return res.json(result);
+  } catch (err) {
+    console.error('Error creating note', err);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
