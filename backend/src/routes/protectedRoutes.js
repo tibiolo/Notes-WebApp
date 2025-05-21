cedef123c5c12e6d1c4286ca77a839f22a6811a6
tibiolo <makso544g@gmail.com>
2025-05-21 02:47:32 +0100

    added /notes  protected routes for fetching and creating notes

--- backend/src/routes/protectedRoutes.js
+++ backend/src/routes/protectedRoutes.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../middleware/authMiddleware.js';
+import { fetchNotes, createNote } from '../controllers/noteController.js';
+import {
+  validateNote,
+  handleValidation,
+} from '../middleware/noteValidation.js';
 
 // Initializing express router
 const router = express.Router();
@@ -8,5 +13,11 @@ const router = express.Router();
 router.get('/dashboard', verifyToken, (req, res) => {
   res.json({ message: `Hello ${req.user.email}, you're authenitcated!` });
 });
+
+// Setting up get notes route
+router.get('/notes', verifyToken, fetchNotes);
+
+// Setting up saving notes
+router.post('/notes', verifyToken, validateNote, handleValidation, createNote);
 
 export default router;
