01f637689b649abe385b08204de5feb98a818b2b
tibiolo <makso544g@gmail.com>
2025-06-03 21:07:52 +0100

    added dockercompose file and renamed env variable name in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Starting the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.BACKEND_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+                                                                  
