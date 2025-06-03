ca9231f685f7f9201cd056a7cbf01968199277e2
tibiolo <makso544g@gmail.com>
2025-06-03 16:59:56 +0100

    fixed frontend not being accessible via localhost when hosted through docker

--- frontend/notes-webapp/vite.config.js
+++ frontend/notes-webapp/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+
   plugins: [react(), tailwindcss()],
 });
